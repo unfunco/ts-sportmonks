5bbf2d62e7a727a937f33bf9b6d28eafcc6c5e59
Daniel Morris <daniel@honestempire.com>
2022-05-02 10:38:35 +0100

    Fix a failing unit test

--- __tests__/authenticationhandler.test.ts
+++ __tests__/authenticationhandler.test.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { AuthenticationHandler } from '..'
+import { AuthenticationHandler } from '../lib'
 
 describe('AuthenticationHandler', () => {
   let authenticationHandler: AuthenticationHandler
